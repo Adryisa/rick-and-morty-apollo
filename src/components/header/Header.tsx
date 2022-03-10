50ac276cf89f12464d059cabcb6543e27682a34c
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-10 16:08:27 +0100

    Add navlinks with main titles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Header(): JSX.Element {
   return (
     <div>
       <h1>Rick and Morty ft. GraphQL</h1>
-      <img src="/assets/rm.png" alt="" height="80px" />
+      <Link to="/*">
+        <img src="/assets/rm.png" alt="" height="80px" />
+      </Link>
     </div>
   );
 }
