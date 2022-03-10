b3f5bfcdfb681bef77526467551f61e291a4e8a7
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-10 16:59:02 +0100

    Add Characters data interface

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
+interface CharacterDataI {
+  characters: {
+    info: {
+      pages: number;
+      next: number;
+      prev: number;
+    };
+    results: [
+      {
+        id: string;
+        name: string;
+        image: string;
+      }
+    ];
+  };
+}
 const CHARACTER_DATA_QUERY = gql`
   query CharacterdDataQuery($page: Int, $filter: FilterCharacter) {
     characters(page: $page, filter: $filter) {
