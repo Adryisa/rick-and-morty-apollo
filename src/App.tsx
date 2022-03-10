b4445ba0d1dbfe40383bceec632d65dc19c02c13
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-10 15:32:21 +0100

    Add header component with img

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/header/Header';
 
 function App(): JSX.Element {
-  return <div className="App" />;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Header(): JSX.Element {
+  return (
+    <div>
+      <h1>Rick and Morty ft. GraphQL</h1>
+      <img src="/assets/rm.png" alt="" height="80px" />
+    </div>
+  );
+}
