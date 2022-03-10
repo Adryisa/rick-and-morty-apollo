15197a0739a380a78f18939269e7a0a48ead0cab
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-10 16:44:26 +0100

    Add characters data query

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const CHARACTER_DATA_QUERY = gql`
+  query CharacterdDataQuery($page: Int, $filter: FilterCharacter) {
+    characters(page: $page, filter: $filter) {
+      info {
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
 
 export function Characters(): JSX.Element {
   return <div>holi soy lista de personajes</div>;
