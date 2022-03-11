6be09f8eb7611f989845d98f43dc6321f9dc1834
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-11 19:39:43 +0100

    Add refactor interfaces in characters pages

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
+interface CharacterPagesDataI {
+  characters: {
+    info:
+      | {
+          count: number | null;
+          pages: number | null;
+          next: number | null;
+          prev: number | null;
+        }
+      | undefined;
+  };
+}
+
+interface CharacterDataI {
+  characters: {
+    results: [
+      | {
+          id: string;
+          name: string;
+          image: string;
+        }
+      | undefined
+    ];
+  };
+}
+
 const CHARACTER_PAGES_QUERY = gql`
   query CharacterPagesQuery($page: Int) {
     characters(page: $page) {
