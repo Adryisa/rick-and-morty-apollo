a58080a6ef65dd3da345749ca1282365ddce7c7d
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-12 13:59:35 +0100

    Add characters details variables and update details query

--- src/pages/characterDetails/CharacterDetails.tsx
+++ src/pages/characterDetails/CharacterDetails.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 
-const CHARACTER_DETAILS_QUERY = gql`
+export const CHARACTER_DETAILS_QUERY = gql`
   query CharacterDetailsQuery($characterId: ID!) {
     character(id: $characterId) {
-      id
       name
       status
       species
@@ -14,17 +13,70 @@ const CHARACTER_DETAILS_QUERY = gql`
       origin {
         id
         name
-        type
-        dimension
+      }
+      location {
+        id
+        name
       }
       image
-      created
+      episode {
+        id
+        name
+      }
     }
   }
 `;
 
+export interface CharactersDetailsI {
+  character: {
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+      id: string;
+      name: string;
+    };
+    location: {
+      id: string;
+      name: string;
+    };
+    image: string;
+    episode: {
+      id: string;
+      name: string;
+    }[];
+  };
+}
+
+interface CharacterDetailsVariableI {
+  characterId?: string;
+}
 export function CharacterDetails(): JSX.Element {
   const { id } = useParams();
 
-  return <div>Holi soy detalles</div>;
+  const { data, loading, error } = useQuery<CharactersDetailsI>(
+    CHARACTER_DETAILS_QUERY,
+    {
+      variables: { characterId: id },
+    }
+  );
+
+  return (
+    <div>
+      {data && (
+        <ul>
+          <img src={data.character.image} alt={data.character.name} />
+          <li>{data.character.name}</li>
+          <li>Vive en: {data.character.origin.name}</li>
+          <li>{data.character.origin.type}</li>
+          <li>{data.character.origin.dimension}</li>
+          <li>{data.character.species}</li>
+          <li>{data.character.status}</li>
+          <li>{data.character.gender}</li>
+        </ul>
+      )}
+    </div>
+  );
 }
