e7294e879153547962384901e0390d3a51961f97
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-12 15:11:44 +0100

    Add gallery interfaces

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
+    <MockedProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </MockedProvider>
   );
   const linkElement = screen.getByText(/Rick and Morty ft. GraphQL/i);
   expect(linkElement).toBeInTheDocument();
