18460795cc8478b5a7f365f144fe81a38374da85
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-14 11:00:41 +0100

    Add characters test with data loaded

--- src/pages/characters/Characters.test.tsx
+++ src/pages/characters/Characters.test.tsx
@@ -1,38 +1,95 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
 import { CHARACTERS_QUERY } from '../../containers/gallery/Gallery';
+import { Characters } from './Characters';
 
 const mocks = [
   {
-    request: CHARACTERS_QUERY,
-    variables: {
-      page: 1,
-      filter: {
-        name: '',
-        gender: '',
-        status: '',
+    request: {
+      query: CHARACTERS_QUERY,
+      variables: {
+        page: 1,
+        filter: {
+          name: '',
+          gender: '',
+          status: '',
+        },
       },
     },
     result: {
       data: {
+        characters: {
+          results: [
+            {
+              id: '1',
+              name: 'ricky',
+              image: 'ricky.png',
+            },
+            {
+              id: '2',
+              name: 'morty',
+              image: 'morty.png',
+            },
+            {
+              id: '3',
+              name: 'bubba',
+              image: 'bubba.png',
+            },
+          ],
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: CHARACTERS_QUERY,
+      variables: {
+        page: 2,
+        filter: {
+          name: '',
+          gender: '',
+          status: '',
+        },
+      },
+    },
+
+    result: {
+      data: {
         results: [
           {
-            id: '1',
-            name: 'ricky',
-            image: 'ricky.png',
+            id: '6',
+            name: 'rickyki',
+            image: 'rickyki.png',
           },
           {
-            id: '2',
-            name: 'morty',
-            image: 'morty.png',
+            id: '7',
+            name: 'moimoi',
+            image: 'moimoi.png',
           },
           {
-            id: '3',
-            name: 'bubba',
-            image: 'bubba.png',
+            id: '8',
+            name: 'buba-test',
+            image: 'buba-test.png',
           },
         ],
       },
     },
   },
 ];
+
+describe('Given the characters component', () => {
+  describe('When the info is loaded', () => {
+    test('Then should render all the characters', async () => {
+      render(
+        <MockedProvider mocks={mocks}>
+          <MemoryRouter>
+            <Characters />
+          </MemoryRouter>
+        </MockedProvider>
+      );
+      expect(await screen.findByText(/ricky/i)).toBeInTheDocument();
+    });
+  });
+});
