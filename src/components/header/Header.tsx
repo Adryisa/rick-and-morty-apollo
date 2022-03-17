c8679e32d1782b5043c659a12bc5dc41bd9a4477
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-17 22:52:04 +0100

    Add header mobile first

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export function Header(): JSX.Element {
   return (
-    <div>
-      <h1>Rick and Morty ft. GraphQL</h1>
+    <div className="flex p-2">
+      <h1 className="hidden">Rick and Morty ft. GraphQL</h1>
       <Link to="/">
-        <img src="/assets/rm.png" alt="rick-and-morty" />
+        <img src="/assets/rm.png" alt="rick-and-morty" className="w-16" />
       </Link>
     </div>
   );
