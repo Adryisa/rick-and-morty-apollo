424ab99958c14353d77617a912a50e03a26da0cf
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-18 15:40:56 +0100

    Update footer adding padding to seeparate

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Footer(): JSX.Element {
   return (
-    <div className="text-pink-100 flex justify-center font-light">
+    <div className="text-pink-100 flex justify-center font-light pt-10 pb-5">
       Made with 💜 by Adry
     </div>
   );
