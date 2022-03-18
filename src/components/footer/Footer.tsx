00832ecc749b98dfe62ccecd24e4e4c86e41f29b
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-03-18 14:33:23 +0100

    Add footer style

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export function Footer(): JSX.Element {
-  return <div className="text-pink-100">With 💜 made by Adry</div>;
+  return (
+    <div className="text-pink-100 flex justify-center font-light">
+      Made with 💜 by Adry
+    </div>
+  );
 }
